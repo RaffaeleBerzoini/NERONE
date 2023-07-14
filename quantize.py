05d28e22c45fd9b469896593e518341ca277cfee
RaffaeleBerzoini <raffaele.berzoini@mail.polimi.it>
2023-07-15 00:02:51 +0200

    quantization ok

---
+++ quantize.py
@@ -0,0 +1,113 @@
+'''
+ Copyright 2020 Xilinx Inc.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+'''
+
+'''
+Quantize the floating-point model
+'''
+
+'''
+Author: Mark Harvey
+'''
+
+
+import argparse
+import os
+import shutil
+import sys
+
+# Silence TensorFlow messages
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
+
+import tensorflow as tf
+from tensorflow_model_optimization.quantization.keras import vitis_quantize
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import load_model
+
+DIVIDER = '-----------------------------------------'
+
+
+
+def quant_model(float_model,quant_model,batchsize,image_directory,colormode,nr_images):
+    '''
+    Quantize the floating-point model
+    Save to HDF5 file
+    '''
+
+    # make folder for saving quantized model
+    head_tail = os.path.split(quant_model) 
+    os.makedirs(head_tail[0], exist_ok = True)
+
+    # load the floating point trained model
+    float_model = load_model(float_model, compile=False)
+
+    # get input dimensions of the floating-point model
+    height = float_model.input_shape[1]
+    width = float_model.input_shape[2]
+
+    # build calibration dataset
+    print(image_directory)
+    total_images = len([name for name in os.listdir(image_directory) if os.path.isfile(os.path.join(image_directory, name))])
+    print(total_images)
+    split = nr_images/total_images
+    
+    data_gen = ImageDataGenerator(validation_split=split)
+    calibration_dataset = data_gen.flow_from_directory(directory=os.path.dirname(image_directory),
+                                                       target_size=(height, width),
+                                                       color_mode=colormode,
+                                                       batch_size=batchsize,
+                                                       subset='validation')
+
+    # run quantization
+    quantizer = vitis_quantize.VitisQuantizer(float_model)
+    quantized_model = quantizer.quantize_model(calib_dataset=calibration_dataset)
+
+    # saved quantized model
+    quantized_model.save(quant_model)
+    print('Saved quantized model to ',quant_model)
+
+
+
+def main():
+
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-m', '--float_model',  type=str, default='build/float_model/f_model.h5', help='Full path of floating-point model. Default is build/float_model/k_model.h5')
+    ap.add_argument('-q', '--quant_model',  type=str, default='build/quant_model/q_model.h5', help='Full path of quantized model. Default is build/quant_model/q_model.h5')
+    ap.add_argument('-b', '--batchsize',    type=int, default=32, help='Batchsize for quantization. Default is 32')
+    ap.add_argument('-d', '--dataset',      type=str, default='build/calibration_dataset', help='Full path to folder containing TFRecord files. Default is build/tfrecords')
+    ap.add_argument('-n', '--nr_images',    type=int, default=500, help='Number of images for the calibration dataset, default is 500')
+    ap.add_argument('-c', '--colormode',    type=str, default="rgb", help='One of "grayscale", "rgb", "rgba". Default: "rgb". Whether the images will be converted to have 1, 3, or 4 channels.')
+    args = ap.parse_args()  
+
+    print('\n------------------------------------')
+    print('TensorFlow version : ',tf.__version__)
+    print(sys.version)
+    print('------------------------------------')
+    print ('Command line options:')
+    print (' --float_model  : ', args.float_model)
+    print (' --quant_model  : ', args.quant_model)
+    print (' --batchsize    : ', args.batchsize)
+    print (' --dataset      : ', args.dataset)
+    print (' --nr_images    : ', args.nr_images)
+    print (' --colormode    : ', args.colormode)
+    print('------------------------------------\n')
+
+
+    quant_model(args.float_model, args.quant_model, args.batchsize, args.dataset, args.colormode, args.nr_images)
+
+
+if __name__ ==  "__main__":
+    main()
